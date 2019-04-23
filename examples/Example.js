673017289f532ec93c6feca3750307387c9134df
Jmeza081 <jmeza081@gmail.com>
2019-04-23 14:30:26 -0500

    add sample rendering of all icons

--- examples/Example.js
+++ examples/Example.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Icon from '../src/index';
+import { getAvailableIconNames } from '../src/utils/Parser';
 
 class Example extends React.Component {
     constructor(props) {
         super(props);
     }
 
-    componentDidMount() {}
-
     render() {
         return (
             <div>
-                <Icon size={500} name={'office'} color={'blue'} />
+                {getAvailableIconNames().map((iconName, index) => {
+                    return <Icon key={index} size={20} name={iconName} color={'red'} />;
+                })}
             </div>
         );
     }
