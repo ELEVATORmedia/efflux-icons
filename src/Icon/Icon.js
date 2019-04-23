d0099cb4c3002c9da46034680ffb311886f41969
Jmeza081 <jmeza081@gmail.com>
2019-04-23 14:30:50 -0500

    update with new Icon parser implementation

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ICONS } from './IconEnumeration';
-import { getPath } from '../utils/Parser';
+import { buildPaths } from '../utils/Parser';
 
 const Icon = (props) => {
-    let { name, size, color, fill } = props;
-    let dynamicStyle = {
-        path: {
-            fill: fill ? color : 'none',
-            stroke: color,
-            strokeMiterlimit: 4,
-            strokeWidth: 51.2
-        },
-    };
+    let { name, size, color } = props;
+
+    let paths = buildPaths(name, color);
+
+    if(paths.length < 1){
+        return <p>?</p>;
+    }
 
     return (
         <svg width={size} height={size} viewBox="0 0 1024 1024">
-            <path style={dynamicStyle.path} d={getPath(name)} />
+            {paths}
         </svg>
     );
 };
@@ -29,10 +26,8 @@ Icon.propTypes = {
 };
 
 Icon.defaultProps = {
-    name: 'BIN2',
     size: 20,
     color: 'black',
-    fill: false,
 };
 
 export default Icon;
